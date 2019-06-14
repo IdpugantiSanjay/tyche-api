3dfbda40da862c5e9faf766de8606b14f8b1c974
sanjay.idpuganti <sanjay.idpuganti@kjsystems.in>
2019-06-15 00:40:30 +0530

    Added Default Fail Action in Hapi Joi Validation

--- src/validators/auth-route.validators.ts
+++ src/validators/auth-route.validators.ts
@@ -19,7 +19,7 @@ function searchUserRoutePayloadSchema() {
   const schema = Joi.object().keys({
     username: Joi.string()
       .required()
-      .max(28),
+      .max(128),
     password: Joi.string()
       .required()
       .max(28)
@@ -34,7 +34,8 @@ export function createUserRouteValidate(): Hapi.RouteOptionsValidate {
     options: {
       abortEarly: true,
       stripUnknown: true
-    }
+    },
+    failAction: defaultFailAction
   } as Hapi.RouteOptionsValidate;
 }
 
@@ -43,7 +44,14 @@ export function searchUserRouteValidate(): Hapi.RouteOptionsValidate {
     payload: searchUserRoutePayloadSchema(),
     options: {
       abortEarly: true,
-      stripUnknown: true
-    }
+      stripUnknown: true,
+    },
+    failAction: defaultFailAction
   } as Hapi.RouteOptionsValidate;
+}
+
+
+
+export var defaultFailAction: Hapi.Lifecycle.Method = (_request, h, error: any) => {
+  return error.isJoi ? h.response(error.details[0]).takeover().code(500).message(error.details[0]) : h.response(error).takeover();
 }
